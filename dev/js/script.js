665ceb7fa34c238e0b91d54b22544a80c33df731
Yuriy Buranov <Yura962Bur@mail.ru>
2021-07-22 17:31:24 +0400

    feat: add form and advantages. feat: Start tabs. feat:add slider for mobile in advantages block.

--- dev/js/script.js
+++ dev/js/script.js
@@ -7,3 +7,55 @@ window.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('menu-mobile--active');
     });
 });
+
+// let tabs = document.querySelectorAll('.tab');
+// for(let i = 0; i < tabs.length; i++){
+//     tabs[i].addEventListener('click', () => {
+//         clearTabs();
+//         this.classList.add('.tab--active');
+//     });
+// }
+
+// function getActiveClassForTab(activeTav = 0) {
+    
+// }
+
+// function clearTabs() {
+//     tabs.forEach((tab) => {
+//         tab.classList.remove('.tab--active');
+//     })
+// }
+
+$(window).on('load', () => {
+    $('.slider').slick({
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: 
+        '<button type="button" class="slick-arrow slick-prev">' +
+            '<svg class="slick-arrow-svg" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg">' + 
+                '<path class="slick-arrow-img" d="M10 16L3 9L10 2" stroke-linecap="round"/>' + 
+            '</svg>' + 
+        '</button>',
+        nextArrow: 
+        '<button type="button" class="slick-arrow slick-next">'+
+            '<svg class="slick-arrow-svg" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+                '<path class="slick-arrow-img" d="M2 2L9 9L2 16" stroke-linecap="round"/>' +
+            '</svg>' +
+        '</button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+})
